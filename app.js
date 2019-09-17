86222e9ca04353519198e6a36df9e97bdb2601c3
Chris <clissmanux@gmail.com>
2019-09-17 14:38:25 -0600

    Complete Assignmnt 12.13 To-Do Part 2

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 
 function onReady() {
-    const TODOS = [];
+    let toDos = [];
     const ADD_TODO_FORM = document.getElementById('addToDoForm');
+    let id = 0;
    
     function createNewTodo() {
         const NEW_TODO_TEXT = document.getElementById('newToDoText');
         if (!NEW_TODO_TEXT.value) return;
-        TODOS.push({
+        
+        toDos.push({
             title: NEW_TODO_TEXT.value,
-            checked: false
+            checked: false,
+            id
         });
+        
+        id++;
         NEW_TODO_TEXT.value = '';
         renderUI();
     }
@@ -19,15 +24,28 @@ function onReady() {
 
         TODO_LIST.textContent = '';
 
-        TODOS.forEach(todo => {
+        toDos.forEach(todo => {
             const NEW_LI = document.createElement('li');
             const CHECKBOX = document.createElement('input')
-            CHECKBOX.type = 'checkbox';
+            const DELETE_BTN = document.createElement('button')
 
+            CHECKBOX.type = 'checkbox';
+            
             NEW_LI.textContent = todo.title;
+            DELETE_BTN.textContent = 'Delete'
+
+            CHECKBOX.addEventListener('click', () => {
+                toDos[todo.id].checked = !toDos[todo.id].checked
+            })
+            
+            DELETE_BTN.addEventListener('click', () => {
+                toDos = toDos.filter(obj => obj.id != todo.id)
+                renderUI();
+            })
 
             TODO_LIST.appendChild(NEW_LI);
             NEW_LI.appendChild(CHECKBOX);
+            NEW_LI.appendChild(DELETE_BTN);
         });
 
     }
