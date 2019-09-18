32e82e8a62bcef60384569be5ce46841e9a095b8
Chris <clissmanux@gmail.com>
2019-09-18 13:13:35 -0600

    Fix rebase conflicts

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 
 function onReady() {
-    const TODOS = [];
+    let toDos = [];
     const ADD_TODO_FORM = document.getElementById('addToDoForm');
+    let id = 0;
    
     function createNewTodo() {
         const NEW_TODO_TEXT = document.getElementById('newToDoText');
         if (!NEW_TODO_TEXT.value) return;
-        TODOS.push({
+        
+        toDos.push({
             title: NEW_TODO_TEXT.value,
-            checked: false
+            checked: false,
+            id
         });
+        
+        id++;
         NEW_TODO_TEXT.value = '';
         renderUI();
     }
@@ -19,23 +24,35 @@ function onReady() {
 
         TODO_LIST.textContent = '';
 
-        TODOS.forEach(todo => {
+        toDos.forEach(todo => {
             const NEW_LI = document.createElement('li');
             const CHECKBOX = document.createElement('input')
             const LABEL = document.createElement('label');
             const TEXTFIELD = document.querySelector('.mdl-js-textfield')
+            const DELETE_BTN = document.createElement('button')
             
-            CHECKBOX.type = 'checkbox';
-            CHECKBOX.classList.add('mdl-checkbox__input');
             NEW_LI.textContent = todo.title;
             NEW_LI.classList.add('mdl-list__item')
+            NEW_LI.textContent = todo.title;
+            CHECKBOX.type = 'checkbox';
+            CHECKBOX.classList.add('mdl-checkbox__input');
             LABEL.classList.add('mdl-checkbox', 'mdl-js-checkbox', 'mdl-js-ripple-effect');
             TEXTFIELD.classList.remove('is-dirty')
+            DELETE_BTN.textContent = 'Delete'
 
-            NEW_LI.appendChild(CHECKBOX);
+            CHECKBOX.addEventListener('click', () => {
+                toDos[todo.id].checked = !toDos[todo.id].checked
+            })
+            
+            DELETE_BTN.addEventListener('click', () => {
+                toDos = toDos.filter(obj => obj.id != todo.id)
+                renderUI();
+            })
+
             NEW_LI.appendChild(LABEL);
             LABEL.appendChild(CHECKBOX)
             TODO_LIST.appendChild(NEW_LI);
+            NEW_LI.appendChild(DELETE_BTN);
         });
     }
 
